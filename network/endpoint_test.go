a37cd855f15a0f03849ecbebe552a2d43e6ac170
Jaeryn <tsch@microsoft.com>
2023-10-09 22:00:36 +0000

    address comments

--- network/endpoint_test.go
+++ network/endpoint_test.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"testing"
 
+	"github.com/Azure/azure-container-networking/cns"
 	"github.com/Azure/azure-container-networking/netio"
 	"github.com/Azure/azure-container-networking/netlink"
 	"github.com/Azure/azure-container-networking/platform"
@@ -182,7 +183,7 @@ var _ = Describe("Test Endpoint", func() {
 			It("Should be added", func() {
 				// Add endpoint with valid id
 				ep, err := nw.newEndpointImpl(nil, netlink.NewMockNetlink(false, ""), platform.NewMockExecClient(false),
-					netio.NewMockNetIO(false, 0), NewMockEndpointClient(false), []*EndpointInfo{epInfo})
+					netio.NewMockNetIO(false, 0), NewMockEndpointClient(nil), []*EndpointInfo{epInfo})
 				Expect(err).NotTo(HaveOccurred())
 				Expect(ep).NotTo(BeNil())
 				Expect(ep.Id).To(Equal(epInfo.Id))
@@ -194,7 +195,7 @@ var _ = Describe("Test Endpoint", func() {
 					extIf:     &externalInterface{IPv4Gateway: net.ParseIP("192.168.0.1")},
 				}
 				ep, err := nw2.newEndpointImpl(nil, netlink.NewMockNetlink(false, ""), platform.NewMockExecClient(false),
-					netio.NewMockNetIO(false, 0), NewMockEndpointClient(false), []*EndpointInfo{epInfo})
+					netio.NewMockNetIO(false, 0), NewMockEndpointClient(nil), []*EndpointInfo{epInfo})
 				Expect(err).NotTo(HaveOccurred())
 				Expect(ep).NotTo(BeNil())
 				Expect(ep.Id).To(Equal(epInfo.Id))
@@ -205,8 +206,8 @@ var _ = Describe("Test Endpoint", func() {
 			})
 			It("Should be not added", func() {
 				// Adding an endpoint with an id.
-				mockCli := NewMockEndpointClient(false)
-				err := mockCli.AddEndpoints(epInfo, nil)
+				mockCli := NewMockEndpointClient(nil)
+				err := mockCli.AddEndpoints(epInfo)
 				Expect(err).ToNot(HaveOccurred())
 				// Adding endpoint with same id should fail and delete should cleanup the state
 				ep2, err := nw.newEndpointImpl(nil, netlink.NewMockNetlink(false, ""), platform.NewMockExecClient(false),
@@ -218,7 +219,7 @@ var _ = Describe("Test Endpoint", func() {
 			})
 			It("Should be deleted", func() {
 				// Adding an endpoint with an id.
-				mockCli := NewMockEndpointClient(false)
+				mockCli := NewMockEndpointClient(nil)
 				ep2, err := nw.newEndpointImpl(nil, netlink.NewMockNetlink(false, ""), platform.NewMockExecClient(false),
 					netio.NewMockNetIO(false, 0), mockCli, []*EndpointInfo{epInfo})
 				Expect(err).ToNot(HaveOccurred())
@@ -240,15 +241,22 @@ var _ = Describe("Test Endpoint", func() {
 					Endpoints: map[string]*endpoint{},
 				}
 				epInfo := &EndpointInfo{
-					Id:     "768e8deb-eth1",
-					IfName: eth0IfName,
+					Id:      "768e8deb-eth1",
+					IfName:  eth0IfName,
+					NICType: cns.InfraNIC,
 				}
 				ep, err := nw.newEndpointImpl(nil, netlink.NewMockNetlink(false, ""), platform.NewMockExecClient(false),
-					netio.NewMockNetIO(false, 0), NewMockEndpointClient(true), []*EndpointInfo{epInfo})
+					netio.NewMockNetIO(false, 0), NewMockEndpointClient(func(ep *EndpointInfo) error {
+						if ep.NICType == cns.InfraNIC {
+							return NewErrorMockEndpointClient("AddEndpoints Infra NIC failed")
+						}
+
+						return nil
+					}), []*EndpointInfo{epInfo})
 				Expect(err).To(HaveOccurred())
 				Expect(ep).To(BeNil())
 				ep, err = nw.newEndpointImpl(nil, netlink.NewMockNetlink(false, ""), platform.NewMockExecClient(false),
-					netio.NewMockNetIO(false, 0), NewMockEndpointClient(false), []*EndpointInfo{epInfo})
+					netio.NewMockNetIO(false, 0), NewMockEndpointClient(nil), []*EndpointInfo{epInfo})
 				Expect(err).NotTo(HaveOccurred())
 				Expect(ep).NotTo(BeNil())
 				Expect(ep.Id).To(Equal(epInfo.Id))
