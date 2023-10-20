fb70fd0b06d6254ecdd64a2c1554402cc0edf0c4
Jaeryn <tsch@microsoft.com>
2023-10-20 23:06:01 +0000

    fix lint

--- network/endpoint_windows.go
+++ network/endpoint_windows.go
@@ -64,7 +64,15 @@ func ConstructEndpointID(containerID string, netNsPath string, ifName string) (s
 }
 
 // newEndpointImpl creates a new endpoint in the network.
-func (nw *network) newEndpointImpl(cli apipaClient, _ netlink.NetlinkInterface, plc platform.ExecClient, _ netio.NetIOInterface, _ EndpointClient, _ NamespaceClientInterface, epInfo []*EndpointInfo) (*endpoint, error) {
+func (nw *network) newEndpointImpl(
+	cli apipaClient,
+	_ netlink.NetlinkInterface,
+	plc platform.ExecClient,
+	_ netio.NetIOInterface,
+	_ EndpointClient,
+	_ NamespaceClientInterface,
+	epInfo []*EndpointInfo,
+) (*endpoint, error) {
 	// there is only 1 epInfo for windows, multiple interfaces will be added in the future
 	if useHnsV2, err := UseHnsV2(epInfo[0].NetNsPath); useHnsV2 {
 		if err != nil {
